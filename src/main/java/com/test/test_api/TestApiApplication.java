46e482289d8d585e94547b1c618e6c06ef9efadf
Fr33zingH3ll <samuelnicod66@gmail.com>
2025-11-11 20:12:12 +0100

    prob driver MariaDB undefined.

--- src/main/java/com/test/test_api/TestApiApplication.java
+++ src/main/java/com/test/test_api/TestApiApplication.java
@@ -7,6 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApiApplication {
 
 	public static void main(String[] args) {
+		try {
+			// Force le chargement du driver MariaDB
+			Class.forName("org.mariadb.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.err.println("MariaDB Driver not found: " + e.getMessage());
+		}
 		SpringApplication.run(TestApiApplication.class, args);
 	}
 
--- src/main/resources/db/changelog/produit.sql
+++
@@ -1,6 +0,0 @@
--- changeset pascal:1
-CREATE TABLE produit (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(255) NOT NULL,
-    prix DECIMAL(10,2)
-);
